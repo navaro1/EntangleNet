71d6f4dac683430fd0bc1f3b033447cad1651692
Piotr Lewandowski <piotr.w.lewandowski@gmail.com>
2024-03-07 09:21:26 +0100

    Minimal setup of test for external_phase

---
+++ synchronous.py
@@ -0,0 +1,16 @@
+from networkx import grid_2d_graph
+
+# graph dimension
+n = 2
+
+network = grid_2d_graph(n, n)
+
+# `p` in the paper
+entanglement_generation_success_probability = 1.0
+
+# `q` in the paper
+entanglement_swap_success_probability = 1.0
+
+# external phase
+
+# internal phase
---
+++ synchronous_test.py
@@ -0,0 +1,2 @@
+def test_external_phase():
+    assert 2 == 2

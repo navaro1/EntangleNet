fd52e80433a870d63824ff2a92b79dcbd4bfb8ec
Piotr Lewandowski <piotr.w.lewandowski@gmail.com>
2024-03-08 12:05:00 +0100

    Add check for lattice graph

--- synchronous.py
+++ synchronous.py
@@ -49,3 +49,55 @@ def internal_phase(instant_topology, source, target, q):
                 paths_queue.append(new_path)
 
     return None
+
+
+def approx_mean_path_length_for_2d_lattice(graph, source, target, check_is_lattice=True):
+    if check_is_lattice and not is_2d_lattice_graph(graph):
+        raise ValueError("Graph is not a 2D lattice")
+
+    pass
+
+
+def is_2d_lattice_graph(graph) -> bool:
+    if nx.utils.graphs_equal(graph, nx.empty_graph()):
+        return True
+    degree_count = nx.degree_histogram(graph)
+    if degree_count == [1]:  # 1 x 1 lattice
+        return True
+
+    if len(degree_count) < 2:  # Lattice with more than 1 node will have at least 2 degrees
+        return False
+
+    nodes_count = graph.number_of_nodes()
+    if degree_count[1] == 2:  # We are in a 1 x N lattice:
+        not_edge_nodes_count = nodes_count - 2
+        return (len(degree_count) == 2  # that is for 1 x 2 lattice
+                or (degree_count == [0, 2, not_edge_nodes_count]))  # that is for 1 x N lattice
+
+    if len(degree_count) == 3 or len(degree_count) == 4:  # We are in a 2 x N lattice:
+        not_corner_nodes_count = nodes_count - 4
+        return (degree_count == [0, 0, 4]  # that is for 2 x 2 lattice
+                or degree_count == [0, 0, 4, not_corner_nodes_count])  # that is for 2 x N lattice
+
+    if len(degree_count) == 5:  # We are potentially in N x M lattice:
+        has_four_corners = degree_count[:3] == [0, 0, 4]
+        if not has_four_corners:
+            return False
+        has_good_number_of_nodes = degree_count[3] + degree_count[4] == nodes_count - 4
+        if not has_good_number_of_nodes:
+            return False
+        decompositions = factor_decomposition(nodes_count)
+        for rows, cols in decompositions:
+            expected_number_of_degree_3_nodes = 2 * (cols - 2) + 2 * (rows - 2)
+            expected_number_of_degree_4_nodes = nodes_count - 4 - expected_number_of_degree_3_nodes
+            if degree_count == [0, 0, 4, expected_number_of_degree_3_nodes, expected_number_of_degree_4_nodes]:
+                return True
+    return False
+
+
+def factor_decomposition(n):
+    factors = []
+    for i in range(1, int(n ** 0.5) + 1):
+        if n % i == 0:
+            factors.append((i, n // i))
+    return factors
--- synchronous_test.py
+++ synchronous_test.py
@@ -3,7 +3,7 @@ import random
 
 import pytest
 
-from synchronous import external_phase, internal_phase
+from synchronous import external_phase, internal_phase, is_2d_lattice_graph
 import networkx as nx
 
 random.seed(100)  # set seed for reproducible tests
@@ -52,7 +52,7 @@ class TestExternalPhase:
 class TestInternalPhase:
     source = (0, 0)  # upper left corner
     target = (2, 2)  # lower right corner
-    full_instant_topology = nx.grid_2d_graph(3, 3)
+    full_instant_topology = nx.grid_2d_graph(5, 5)
 
     def test_should_return_none_when_instant_topology_is_empty(self):
         path = internal_phase(nx.empty_graph(), source=self.source, target=self.target, q=1.0)
@@ -90,3 +90,54 @@ class TestInternalPhase:
         disconnected_instant_topology.add_node(3)
         path = internal_phase(disconnected_instant_topology, source=0, target=3, q=1)
         assert path is None
+
+
+class TestUtils:
+    def test_should_return_true_for_empty_graph(self):
+        assert is_2d_lattice_graph(nx.empty_graph())
+
+    def test_should_return_false_for_disconnected_graph(self):
+        g = nx.empty_graph()
+        g.add_node(1)
+        g.add_node(2)
+        assert not is_2d_lattice_graph(g)
+
+    def test_should_return_false_for_tree_graph(self):
+        g = nx.path_graph(3)
+        g.add_node(3)
+        g.add_edge(1, 3)
+        assert not is_2d_lattice_graph(g)
+
+    @pytest.mark.parametrize("n,m", [(1, 1), (1, 2), (1, 3), (2, 2), (2, 3), (3, 3), (7, 7), (2, 4), (1, 7), (3, 7),
+                                     (100, 100)])
+    def test_should_return_true_for_2d_lattice_graph(self, n, m):
+        g = nx.grid_2d_graph(n, m)
+        assert is_2d_lattice_graph(g)
+
+
+class TestExternalInternalPhases:
+    @pytest.mark.skip(reason="MissXing approx of paths")
+    def test_should_be_close_to_solutions_from_paper_on_2d_graph():
+        # based on the paper
+        full_instant_topology = nx.grid_2d_graph(5, 5)
+        p = 0.8
+        q = 0.8
+        source = (0, 0)
+        target = (4, 4)
+        disjoint_paths = list(nx.edge_disjoint_paths(full_instant_topology, s=source, t=target))
+        all_paths_lengths = [len(p) - 1 for p in
+                             nx.all_shortest_paths(full_instant_topology, source=source, target=target)]
+        mean_path_length = sum(all_paths_lengths) / len(all_paths_lengths)
+        number_of_disjoint_paths = len(disjoint_paths)
+        expected_rate = p ** mean_path_length * q ** (mean_path_length - 1) * number_of_disjoint_paths
+
+        rates = []
+        for n in range(1000):
+            instant_topology = external_phase(full_instant_topology, p)
+            path = internal_phase(instant_topology, source=source, target=target, q=q)
+            if path is None:
+                rates.append(0)
+            else:
+                rates.append(1)
+        actual_rate = sum(rates) / len(rates)
+        assert math.isclose(actual_rate, expected_rate)
